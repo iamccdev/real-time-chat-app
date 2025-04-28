1d37251a6663a2278233e3dd6cfa928044cb60c1
iamccdev <iamccdev@gmail.com>
2025-04-28 07:05:52 -0500

    issues resolved

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
-import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
+import { Eye, EyeOff, Loader2, Mail, MessageSquare, User } from "lucide-react";
 import { Link } from "react-router-dom";
 
 import toast from "react-hot-toast";
